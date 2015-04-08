be7818fdb4df55e750b6282a8802d47b95b97040
Max Heiber <max.heiber@gmail.com>
2015-04-08 18:19:33 -0400

    Sort of works
    Still dropping 11K zip codes.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from write_closest import write_closest
 from factify import factify
+from clean_results import clean_results
 from csv_to_json import *
 
 
-fact_file='201503daily.csv'
+fact_file='201502daily.csv'
 zip_codes_file='./zipLatLonNormalized.csv'
 wbans_file='./LU_WBAN.csv'
 
-closest_wbans_file='closest_v3.csv'
+dirty_results_file='results_results_201502.csv'
+clean_results_file='results_clean_201502.csv'
+
+closest_wbans_file='closest_201502.csv'
 
 fact_rows=csv_to_list_of_dicts(fact_file)
 zip_codes=csv_to_list_of_dicts(zip_codes_file)
@@ -18,9 +22,15 @@ with open('config.json','r') as f:
 
 required_fields=config['required_fields']
 
-
-
+#fact_file is CSV of format WBAN, facts ...
+#wbans file is CSV of format WBAN, lat, lon
+#output is closest_wbans_file of format zip,lat_lon,closest_wban_id
 write_closest(fact_file,required_fields,zip_codes_file,wbans_file,closest_wbans_file)
 
-factify(fact_file,closest_wbans_file,'results_results.csv')
+# dirty_results_file is of format YearMonthDay,zip_code, facts
+#inconsistent indication of null values, though: sometimes 'M', sometimes blank
+factify(fact_file,closest_wbans_file,dirty_results_file)
+
+#cleaned output has None values for nulls
+clean_results(dirty_results_file,clean_results_file)
 
--- write_closest.py
+++ write_closest.py
@@ -1,77 +1,69 @@
 
 import json
 from csv_to_json import *
-
+from utils import is_empty
 from geopy.distance import great_circle as get_distance
+
+
+def _get_distinct_wbans(wbans_from_fact):
+	distinct_wban_ids=reduce(lambda distinct_list,wban: distinct_list+[wban['WBAN']] if wban['WBAN'] not in distinct_list else distinct_list, wbans_from_fact,[])
+	wbans=filter(lambda wban: wban['WBAN'] in distinct_wban_ids,wbans_from_fact)
+	return wbans
+
+def _location(dictionary):
+	return (dictionary['lat'],dictionary['lon'])
+
+def _distance_between(dictionary1,dictionary2):
+	location1=_location(dictionary1)
+	location2=_location(dictionary2)
+	return get_distance(location1,location2)
+
+def _closest_wban_id(lat_lon,wbans):
+	lat_lon['closest_wban']=wbans[0]
+	least_distance=_distance_between(lat_lon,wbans[0])
+	wban_id=wbans[0]['WBAN']
+	for wban in wbans:
+		wban_distance=_distance_between(lat_lon,wban)
+		if wban_distance<least_distance:
+			least_distance=wban_distance
+			wban_id=int(wban['WBAN'])
+	print 'closest is',wban_id
+	return wban_id
 
 def write_closest(fact_file,required_fields,zip_codes_file,wbans_file,out_file):
 
-	def get_distinct_wbans():
-		lat_lons=csv_to_list_of_dicts(zip_codes_file)
-		wbans=csv_to_list_of_dicts(wbans_file)
-
-		wbans_from_fact=csv_to_list_of_dicts(wbans_file)
-		distinct_wbans=reduce(lambda distinct_list,wban: distinct_list+[wban['WBAN']] if wban['WBAN'] not in distinct_list else distinct_list, wbans_from_fact,[])
-		wbans=filter(lambda wban: wban['WBAN'] in distinct_wbans,wbans)
-		return wbans
-
-	def location(dictionary):
-		return (dictionary['lat'],dictionary['lon'])
-
-	def distance_between(dictionary1,dictionary2):
-		location1=location(dictionary1)
-		location2=location(dictionary2)
-		return get_distance(location1,location2)
-
-	def closest_wban_id(lat_lon,wbans):
-		lat_lon['closest_wban']=wbans[0]
-		least_distance=distance_between(lat_lon,wbans[0])
-		wban_id=wbans[0]['WBAN']
-		for wban in wbans:
-			wban_distance=distance_between(lat_lon,wban)
-			if wban_distance<least_distance:
-				least_distance=wban_distance
-				wban_id=int(wban['WBAN'])
-		print 'closest is',wban_id
-		return wban_id
-
-
-	def is_empty(value):
-		return value in [None,'','M','m',' ','  ','\t']
-
-	def is_valid_row(row):
+	#closes over required_fields
+	def _is_valid_row(row):
 		for field in required_fields:
 			if is_empty(row[field]):
 				return False
 		return True
 
-
-	def valid_wban_id_reducer(valids,fact_row):
-		return valids+[fact_row['WBAN']] if fact_row['WBAN'] not in valids else valids
-
-
 	fact_rows=csv_to_list_of_dicts(fact_file)
-	valid_fact_rows=filter(is_valid_row,fact_rows)
-	print 'valid fact rows',len(valid_fact_rows)
-	valid_wban_ids=reduce(valid_wban_id_reducer,valid_fact_rows,[])
-	print 'valids length',len(valid_wban_ids)
-	#closes over three vars above
-	def is_valid_wban(wban):
-		return wban['WBAN'] in valid_wban_ids
+	valid_fact_rows=filter(_is_valid_row,fact_rows)
+	wbans=csv_to_list_of_dicts(wbans_file)
 
-	distinct_wbans=get_distinct_wbans()
+
+	def _is_valid_wban(wban):
+		for fact_row in fact_rows:
+			if fact_row['WBAN']==wban['WBAN'] and not _is_valid_row(fact_row):
+				print 'invalid wban',fact_row
+				return False
+		return True
+
+	
+	distinct_wbans=_get_distinct_wbans(wbans)
 	print 'distinct', len(distinct_wbans)
-	wbans=filter(is_valid_wban,distinct_wbans)	#We now have a list of WBANs that have values for all
-
+	wbans=filter(_is_valid_wban,distinct_wbans)
+	#We now have a list of WBANs that have values for all
 	#required fields for the whole month
 
 	print 'got list of valid wbans',len(wbans)
 	zip_codes=csv_to_list_of_dicts(zip_codes_file)
 	for zip_code in zip_codes:
-		zip_code['closest_wban_id']=closest_wban_id(zip_code,wbans)
+		zip_code['closest_wban_id']=_closest_wban_id(zip_code,wbans)
 
 	list_of_dicts_to_csv(zip_codes,out_file)
-
 
 
 
